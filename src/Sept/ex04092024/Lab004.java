fcb3d225a4a81c89aa1260811ca120840a165299
pallavisharma3024 <pallavisharma28p@gmail.com>
2024-09-09 16:01:32 +0530

    9th Sep 2024

---
+++ src/Sept/ex04092024/Lab004.java
@@ -0,0 +1,22 @@
+package Sept.ex04092024;
+
+public class Lab004
+{
+
+    public static void main (String [] args)
+    {
+        System.out.println("He");
+
+        boolean age_verified= true;
+
+        if (age_verified== false)
+        {
+            System.out.println("Plesae verify the age 1st");
+        }
+        else
+            System.out.println("Verified");
+    }
+
+
+
+}
---
+++ src/Sept/ex09092024/Lab004.java
@@ -0,0 +1,19 @@
+package Sept.ex09092024;
+
+public class Lab004 {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("He");
+
+        boolean age_verified= false;
+
+        if (age_verified== false)
+        {
+            System.out.println("Plesae verify the age 1st");
+        }
+        else
+            System.out.println("Verified");
+    }
+}
