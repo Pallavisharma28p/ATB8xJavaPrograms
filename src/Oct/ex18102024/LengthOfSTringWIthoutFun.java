45bbe985a5d2c5492f611c65f1d869d11f2ecc87
pallavisharma3024 <pallavisharma28p@gmail.com>
2024-10-18 12:36:49 +0530

    18th Oct 2024

---
+++ src/Oct/ex18102024/LengthOfSTringWIthoutFun.java
@@ -0,0 +1,10 @@
+package Oct.ex18102024;
+
+public class LengthOfSTringWIthoutFun {
+
+    public static void main(String[] args) {
+        String name="laosndpsnoi";
+        System.out.println(name.toCharArray().length);
+        System.out.println(name.lastIndexOf(""));
+    }
+}
